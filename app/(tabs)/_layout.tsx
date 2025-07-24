39406ac9a58d5ed06e1606effc0893414ac12dbb
liat g <liatg812@gmail.com>
2025-07-24 16:50:08 -0400

    now having problems with tsx/jsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,42 +2,85 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from 'react';
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 const TabIcon = ({focused, icon, title}: any) => {
+    if (focused) {
+//focused prop is for the state of onClick 
     return (
         <ImageBackground 
         source={images.highlight}
         className="flex flex-row w-full 
-        flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center
-        rounded-full overflow-hidden"
+        flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center
+        rounded -full overflow-hidden"
     
     >
     
-        <Image source={icons.home} 
-    tintColor="#151312" className="size-5" />
+        <Image source={icon} tintColor="#151312" 
+    className="size-5" />
         <Text
     className="text-secondary text-base font-semibold
     ml-2"
     >
-        Home 
+       {title}
     </Text>
         </ImageBackground>
+    )
+    }
+    
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor="#A8B5DB" 
+    className="size-5" />
+        </View>
+
     )
 }
 
 const _layout = () => {
+
+
   return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center"
+            },
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50, 
+                marginHorizontal: 20,
+                marginBottom: 36, 
+                height: 52, 
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '0f0d23'
+
+
+            }
+
+            }}
+        
+        
+        
+        >
             <Tabs.Screen 
             name="index"
             options={{
                 title: 'Home',
                 headerShown: false,
                 tabBarIcon: ({ focused }) => (
-            <TabIcon />
-        )
-    }}
-    />
+                <TabIcon 
+                    focused={focused}
+                    icon={icons.home}
+                    title="Home"
+                    />
+                )
+            }}/>
     <Tabs.Screen 
     name="search"
     options={{
@@ -46,8 +89,8 @@ const _layout = () => {
         tabBarIcon: ({ focused }) => (
         <TabIcon 
             focused={focused}
-            icon={icons.home}
-            title="Home"
+            icon={icons.search}
+            title="Search"
             />
         )
     }}
@@ -73,7 +116,7 @@ const _layout = () => {
         tabBarIcon: ({ focused }) => (
             <TabIcon 
             focused={focused}
-            icon={icons.person}
+            icon={icons.profile}
             title="Profile" />
         )
     }}
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,16 +1,16 @@
-import home from "@/assets/icons/home.png";
-import search from "@/assets/icons/search.png";
-import person from "@/assets/icons/person.png";
-import logo from "@/assets/icons/logo.png";
-import save from "@/assets/icons/save.png";
-import star from "@/assets/icons/star.png";
-import play from "@/assets/icons/play.png";
 import arrow from "@/assets/icons/arrow.png";
+import home from "@/assets/icons/home.png";
+import logo from "@/assets/icons/logo.png";
+import play from "@/assets/icons/play.png";
+import profile from "@/assets/icons/profile.png";
+import save from "@/assets/icons/save.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
 
 export const icons = {
   home,
   search,
-  person,
+  profile,
   logo,
   save,
   star,
