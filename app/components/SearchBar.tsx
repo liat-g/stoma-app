49aaae8e7b9db34a91142ada2d8685e02939001d
liat g <liatg812@gmail.com>
2025-07-25 11:39:59 -0400

    added searchbar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,10 +1,19 @@
+import { icons } from '@/constants/icons'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, TextInput, View } from 'react-native'
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className="flex-row item-center bg-dark-200 rounded-full px-5 py-4">
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+      <TextInput 
+        onPress={() => {}}
+        placeholder="Search"
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+        />
     </View>
   )
 }
